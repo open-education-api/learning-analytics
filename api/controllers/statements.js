c2fcfc5f2bb75d2769e2900de97802862c76f1c4
tomkuipers <t.f.kuipers@uva.nl>
2016-01-22 14:53:23 +0100

    proxy create statement post

--- api/controllers/statements.js
+++ api/controllers/statements.js
@@ -4,7 +4,8 @@ var request = require('request');
 
 module.exports = {
     listStatements: listStatements,
-    getStatementById: getStatementById
+    getStatementById: getStatementById,
+    createStatement: createStatement
 };
 
 var headers = {
@@ -20,4 +21,41 @@ function listStatements(req, res) {
 function getStatementById(req, res) {
     var statementId = req.swagger.params.statementId.value;
     req.pipe(request.get({url: 'http://localhost:8080/xAPI/statements?statementId=' + statementId, headers: headers})).pipe(res);
+}
+
+function createStatement(req, res) {
+    var statement = req.swagger.params.body.value;
+
+    request.post({url: 'http://localhost:8080/xAPI/statements', headers: headers, body:statement}).pipe(res);
+
+    /*
+    Example statement:
+
+    {
+        "actor": {
+        "mbox": "mailto:darlene.snyder95@example.com",
+            "name": "Darlene Snyder",
+            "objectType": "Agent"
+    },
+        "verb": {
+        "id": "https://w3id.org/xapi/adl/verbs/attempted",
+            "display": {
+            "en-US": "attempted"
+        }
+    },
+        "object": {
+        "id": "https://w3id.org/xapi/adl/activities/course",
+            "objectType": "Activity",
+            "definition": {
+            "name": {
+                "en-US": "course"
+            },
+            "description": {
+                "en-US": "course"
+            }
+        }
+    }
+    }
+    */
+
 }
